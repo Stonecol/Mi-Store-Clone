695e79c33f466f209e161cc1cbec9788949b6d92
Prem Kumar <prem71717@gmail.com>
2023-08-26 01:39:57 +0530

    navbar updated

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,86 +5,72 @@ export const Navbar = () => {
   const navigate = useNavigate();
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between full-width">
-          <div class="navbar-nav mt-2 mt-lg-0 d-flex justify-content-between">
-            <div class="nav-item active">
-              <a
-                class="nav-link"
+      <div className="nav-bar">
+        <nav>
+          <div className="wrapper d-flex justify-content-between">
+            <div className="link-wrapper">
+              <div
+                className="nav-item"
                 onClick={() => {
                   navigate("/");
                 }}
-                href="#"
               >
-                Home
-              </a>
-            </div>
-            <div class="nav-item">
-              <a
-                class="nav-link"
+                <a className="c-nav-links" href="#">
+                  Home
+                </a>
+              </div>
+              <div
+                className="nav-item"
                 onClick={() => {
                   navigate("/phone");
                 }}
-                href="#"
               >
-                Phone
-              </a>
-            </div>
-            <div class="nav-item">
-              <a
-                class="nav-link"
-                onClick={() => {
-                  navigate("/smart-office");
-                }}
-                href="#"
-              >
-                Laptop & Tablet
-              </a>
-            </div>
-            <div class="nav-item">
-              <a
-                class="nav-link"
+                <a className="c-nav-links" href="#">
+                  Phone
+                </a>
+              </div>
+              <div
+                className="nav-item"
                 onClick={() => {
                   navigate("/smart-home");
                 }}
-                href="#"
               >
-                TV & Smart Home
-              </a>
-            </div>
-            <div class="nav-item">
-              <a
-                class="nav-link"
+                <a className="c-nav-links" href="#">
+                  TV & Smart Home
+                </a>
+              </div>
+              <div
+                className="nav-item"
+                onClick={() => {
+                  navigate("/smart-office");
+                }}
+              >
+                <a className="c-nav-links" href="#">
+                  Laptop & Tablet
+                </a>
+              </div>
+              <div
+                className="nav-item"
                 onClick={() => {
                   navigate("/life-style");
                 }}
-                href="#"
               >
-                LifeStyle
-              </a>
+                <a className="c-nav-links" href="#">
+                  LifeStyle
+                </a>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="my-icons">
+                <i class="bi bi-cart"></i>
+              </div>
+              <div className="my-icons">
+                <i class="bi bi-person"></i>
+              </div>
             </div>
           </div>
-          <div className="d-flex">
-            <div className="my-icons">
-              <i class="bi bi-cart"></i>
-            </div>
-            <div className="my-icons">
-              <i class="bi bi-person"></i>
-            </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
