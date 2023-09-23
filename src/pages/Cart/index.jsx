e358c58fddec296c152f5ae0e37eb0006fbcea45
Prem Kumar <prem71717@gmail.com>
2023-09-23 22:10:45 +0530

    cart progress status added

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -2,37 +2,20 @@ import { CartItem } from "../../components/CartItem";
 import "./index.css";
 import { useContext } from "react";
 import { AppContext } from "../../context/appContext";
-
-const CartHeader = () => {
-  return (
-    <>
-      <div className="row cart-header-container">
-        <div className="col-lg-2"></div>
-        <div className="col-lg-4">Product Name</div>
-        <div className="col-lg-2">Price</div>
-        <div className="col-lg-2">Quantity</div>
-        <div className="col-lg-2">Total</div>
-      </div>
-    </>
-  );
-};
-
+import { CartHeader } from "../../components/CartHeader";
+import { CartNoItem } from "../../components/CartNoItem";
+import { CartProgressCard } from "../../components/CartProgressCard";
 export const Cart = () => {
   const { appState, setAppState } = useContext(AppContext);
 
   const { cartItems } = appState;
+
   return (
     <>
+      <CartProgressCard />
+
       {cartItems.length === 0 ? (
-        <div className="cart-no-item">
-          <div>
-            <img
-              src="https://i01.appmifile.com/webfile/globalimg/i18n_frontend/cart/cart-empty.svg"
-              alt=""
-            />
-            <p>Your Shopping cart is empty</p>
-          </div>
-        </div>
+        <CartNoItem />
       ) : (
         <>
           <div className="cart-container">
