4e09142af7e29ed8a727013afb1b58a22633b6bb
Prem Kumar <prem71717@gmail.com>
2023-09-28 00:37:53 +0530

    cart checkout

--- src/dummyData/smartTvs.js
+++ src/dummyData/smartTvs.js
@@ -28,10 +28,10 @@ export const smartTvs = [
         name: "Mi TV 4A 40 Horizon",
         price:"18,999"
     },{
-        img: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1663152565.22081554!348x348.jpg?f=webp",
-        discount: 44,
-        name: "Xiaomi Smart TV 5A",
-        price:"13,999"
+        img: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1681292448.74477327!348x348.jpg?f=webp",
+        discount: 34,
+        name: "Xiaomi Smart TV X Pro",
+        price:"32,999"
     },
     {
         img: "https://i03.appmifile.com/251_item_in/15/05/2023/a1e6adb1ce20aea70a6240f16f82fac8!348x348.jpg?f=webp",
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,26 @@
-export const findCartItem = (cartItems,name) => {
-    const item = cartItems.filter((item,index) => item.name === name);
-    return item;
-}
+export const findCartItem = (cartItems, name) => {
+  const item = cartItems.filter((item, index) => item.name === name);
+  return item;
+};
+
+export const findCartTotal = (cartItems) => {
+  let total = 0;
+  cartItems.forEach((item) => {
+    let val = Number(item.price.replace(/[^0-9.-]+/g, ""));
+    total += val * item.qty;
+  });
+  return total;
+};
+
+export const convertProductPrice = (price) => {
+  return Number(price.replace(/[^0-9.-]+/g, ""));
+};
+
+export const formatPrice = (price) => {
+  let formatter = Intl.NumberFormat("INR", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  });
+  return formatter.format(price);
+};
